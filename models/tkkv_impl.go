e3900871b4aee614462465f0827669b3b5596b91
trungthanh <trungthanhnt@gmail.com>
2018-11-14 08:20:20 +0700

    Todo: Please complete dev the model and controller

--- models/tkkv_impl.go
+++ models/tkkv_impl.go
@@ -6,11 +6,31 @@ func NewTrustKeysKVModel() TrustKeysKVModelIf{
 	return nil;
 }
 
-type SimpleTKKVModel struct  {
+type CheckWhiteListPubKey func(string) bool ;
 
+/*SimpleTKKVModel with bigset */
+type SimpleTKKVModel struct  {
+	//function to check if a public key is enabled to store data
+	KeyAvailable CheckWhiteListPubKey
+
+	// Big set host/port for storing data
+	DataBSHost string
+	DataBSPort string 
 }
 
+func acceptAllPubKey(string) bool {
+	return true;
+}
+
+func makeKey(pubKey, key string) string {
+	return pubKey + ":" + key;
+}
+
+
+
 func (o *SimpleTKKVModel) Put(appID, pubKey, key, value string) (ok bool, oldValue, transctionID string ) {
+	bskey := makeKey(pubKey, key);
+	// pub into bigset : appID , bsKey , value 
 	ok = false;
 	return
 }
