b50babb7997fc2f905faa18f35ddc77aa372dae8
trungthanh <trungthanhnt@gmail.com>
2018-11-15 18:30:23 +0700

    ok

--- routers/router.go
+++ routers/router.go
@@ -1,15 +1,14 @@
 // @APIVersion 1.0.0
-// @Title Simple KV API
-// @Description beego has a very cool tools to autogenerate documents for your API and SimpleKV used it
+// @Title Public Key-value store for trustkeys.network
+// @Description An awesome key-value store for mobile application that verify ECDSA digital signature with secp256k1
 // @Contact thanhnt@123xe.vn
-// @TermsOfServiceUrl https://123xe.vn/cms/dieukhoan
+// @TermsOfServiceUrl https://kvpublic.trustkeys.network/swagger/
 // @License Apache 2.0
 // @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
 
 import (
 	"github.com/trustkeys/trustkeyskv/controllers"
-
 	"github.com/astaxie/beego"
 )
 
